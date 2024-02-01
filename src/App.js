a700ec83d3b91b1ead4caec2510c666d823220f3
Anıl <anil@Anl-MacBook-Air.local>
2024-02-01 12:41:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react';
 
-function App() {
+const App = () => {
+  // ref for dealing with js setInterval for keeping track of it and stopping if needed
+  const Ref = useRef(null);
+
+  // usestate hook for timer
+  const [timer, setTimer] = useState('00:00:00');
+
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+    return {
+      total,
+      hours,
+      minutes,
+      seconds,
+    };
+  };
+
+  const startTimer = (e) => {
+    let { total, hours, minutes, seconds } = getTimeRemaining(e);
+    if (total >= 0) {
+      setTimer(
+        (hours > 9 ? hours : '0' + hours) +
+          ':' +
+          (minutes > 9 ? minutes : '0' + minutes) +
+          ':' +
+          (seconds > 9 ? seconds : '0' + seconds)
+      );
+    }
+
+    const clearTimer = (e) => {
+      setTimer('23:59:59');
+      if (Ref.current) clearInterval(Ref.current);
+      const id = setInterval(() => {
+        startTimer(e);
+      }, 1000);
+      Ref.current = id;
+    };
+  };
+
+  const getDeadTime = () => {
+    let deadline = new Date();
+
+    deadline.setSeconds(deadline.getSeconds() + 10);
+    return deadline;
+  };
+
+  useEffect(() => {
+    clearTimer(getDeadTime());
+  }, []);
+
+  const onClickReset = () => {
+    clearTimer(getDeadTime());
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', margin: 'auto' }}>
+      <h1 style={{ color: 'blue' }}>Anil Yavas Countdown Timer</h1>
+      <h3>Countdown Timer With React JS</h3>
+      <h2>{timer}</h2>
+      <button onClick={onClickReset}>Reset</button>
     </div>
   );
-}
+};
 
 export default App;
