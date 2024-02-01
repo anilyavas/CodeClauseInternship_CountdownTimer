a9ad62f1e250c849ea9056260246ca855cdf0ca4
Anıl <anil@Anl-MacBook-Air.local>
2024-02-01 12:59:57 +0300

    countdown timer with seconds

--- src/App.js
+++ src/App.js
@@ -1,72 +1,7 @@
-// Filename - App.js
-
-import React, { useState, useRef, useEffect } from 'react';
+import CountdownTimer from './CountdownTimer';
 
 const App = () => {
-  const Ref = useRef(null);
-
-  // The state for our timer
-  const [timer, setTimer] = useState('00:00:00');
-
-  const getTimeRemaining = (e) => {
-    const total = Date.parse(e) - Date.parse(new Date());
-    const seconds = Math.floor((total / 1000) % 60);
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / 1000 / 60 / 60) % 24);
-    return {
-      total,
-      hours,
-      minutes,
-      seconds,
-    };
-  };
-
-  const startTimer = (e) => {
-    let { total, hours, minutes, seconds } = getTimeRemaining(e);
-    if (total >= 0) {
-      setTimer(
-        (hours > 9 ? hours : '0' + hours) +
-          ':' +
-          (minutes > 9 ? minutes : '0' + minutes) +
-          ':' +
-          (seconds > 9 ? seconds : '0' + seconds)
-      );
-    }
-  };
-
-  const clearTimer = (e) => {
-    setTimer('23:59:59');
-
-    if (Ref.current) clearInterval(Ref.current);
-    const id = setInterval(() => {
-      startTimer(e);
-    }, 1000);
-    Ref.current = id;
-  };
-
-  const getDeadTime = () => {
-    let deadline = new Date();
-
-    deadline.setSeconds(deadline.getSeconds() + 10);
-    return deadline;
-  };
-
-  useEffect(() => {
-    clearTimer(getDeadTime());
-  }, []);
-
-  const onClickReset = () => {
-    clearTimer(getDeadTime());
-  };
-
-  return (
-    <div style={{ textAlign: 'center', margin: 'auto' }}>
-      <h1 style={{ color: 'green' }}>Countdown Timer Anil Yavas</h1>
-      <h3>Countdown Timer Using React JS with Custom Functions</h3>
-      <h2>{timer}</h2>
-      <button onClick={onClickReset}>Reset</button>
-    </div>
-  );
+  return <CountdownTimer initialSeconds={300} />;
 };
 
 export default App;
